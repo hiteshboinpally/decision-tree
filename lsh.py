ef2b5f548ca54bdb9e56d7b1a350082c47ba0054
hiteshb <hitesh.boinpally@gmail.com>
2020-08-16 19:24:36 -0700

    updated with LSH set of sets

--- lsh.py
+++ lsh.py
@@ -3,18 +3,27 @@ import random
 
 
 class LSH:
-    def __init__(self, data):
-        self.shingle_length = 3
-        self.num_permutations = 6
-        self.num_rows_per_band = 2
-        self.num_buckets = 10
+    def __init__(self, data, shingle_length, permutations, num_rows_per_band, num_buckets):
+        self.shingle_length = shingle_length
+        self.num_permutations = permutations
+        self.num_rows_per_band = num_rows_per_band
+        self.num_buckets = num_buckets
         self.num_documents = len(data)
         #perform shingling
         #minhashing
         shingles_document = self.shingling(data)
         sig_matrix = self.min_hash(shingles_document)
         similar_documents = self.lsh(sig_matrix)
-        print(similar_documents)
+        self.sim_docs_set = set()
+        self.get_set_of_sim_docs(similar_documents)
+
+        print(self.sim_docs_set)
+
+
+    def get_set_of_sim_docs(self, similar_documents):
+        for set_docs in similar_documents:
+            if len(set_docs) > 1:
+                self.sim_docs_set.add(frozenset(set_docs))
 
 
     def shingling(self, data):
@@ -69,6 +78,7 @@ class LSH:
                 buckets[bucket_in].add(j)
             for index,docs in buckets.items():
                 if len(docs) > 1:
+                    docs = frozenset(docs)
                     similar_documents[index].update(docs)
         return similar_documents
 
@@ -78,7 +88,7 @@ def main():
     data_two = open(r"hello_two.txt", "r")
     data_three = open(r"hello_three.txt", "r")
     data = [data_one.read(), data_two.read(), data_three.read()]
-    LSH(data)
+    LSH(data, 3, 6, 2, 10)
 
 
 if __name__ == "__main__":
